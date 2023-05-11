5e1b0f4ec16d93f1d6fccf9e41b6969a482e13d7
raffaele-catalano <raf.catalano@gmail.com>
2023-05-11 12:42:42 +0200

    created a db.json for Main Section-1 for Cards dynamic print on page

--- src/components/partials/Main-section-1.vue
+++ src/components/partials/Main-section-1.vue
@@ -1,29 +1,24 @@
 <script>
+import MainSection1db from '../../data/MainSection-1-db.json'
 export default {
     name: 'Cards',
+    data() {
+        return {
+            MainSection1db,
+        }
+    }
 }
 </script>
 
 <template>
 <section class="cards">
     <div class="row row-cols-3">
-        <div class="col p-0">
-            <img src="../../assets/img/img-Main/main-first-section/img-1.jpg" alt="">
-        </div>
-        <div class="col p-0">
-            <img src="../../assets/img/img-Main/main-first-section/img-2.jpg" alt="">
-        </div>
-        <div class="col p-0">
-            <img src="../../assets/img/img-Main/main-first-section/img-3.jpg" alt="">
-        </div>
-        <div class="col p-0">
-            <img src="../../assets/img/img-Main/main-first-section/img-4.jpg" alt="">
-        </div>
-        <div class="col p-0">
-            <img src="../../assets/img/img-Main/main-first-section/img-5.jpg" alt="">
-        </div>
-        <div class="col p-0">
-            <img src="../../assets/img/img-Main/main-first-section/img-6.jpg" alt="">
+        <div class="col p-0" v-for="(card, index) in MainSection1db" :key="index">
+            <img :src="`main-first-section-img/${card.src}.jpg`" :alt="card.src">
+            <div class="description_container">
+                <h3>{{ card.title }}</h3>
+                <p>{{ card.description }}</p>
+            </div>
         </div>
     </div>
 </section>
@@ -37,8 +32,30 @@ export default {
     width: 100%;
     .row{
         .col{
+            position: relative;
+            
+            &:hover {
+                .description_container{
+                    width: 350px;
+                    padding: 10px 20px;
+                    opacity: 1;
+                }
+            }
             //debug
             // border: 2px solid blue;
+            .description_container{
+                display: flex;
+                justify-content: center;
+                flex-direction: column;
+                position: absolute;
+                left: 0;
+                top: 50%;
+                background-color: white;
+                transition: all .6s;
+                width: 0;
+                opacity: 0;
+            }
+
             img {
                 width: 100%;
             }
